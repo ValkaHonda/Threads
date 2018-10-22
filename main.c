95c1c0efa00cadcd4e2247afeb3c228e1204b28b
Lenny <valentin805@gmail.com>
2018-10-22 23:10:43 +0300

    Add more stuff to the git ignore file and new java project

--- main.c
+++ main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+
+void *myThreadFun(void *vargp)
+{
+    sleep(1);
+    printf("This is the thread\n");
+    return NULL;
+}
 
 int main()
 {
-    printf("Hello world!\n");
+    pthread_t thread_id;
+    printf("Before Thread\n");
+    pthread_create(&thread_id, NULL, myThreadFun, NULL);
+    pthread_join(thread_id, NULL);
+    printf("After Thread\n");
     return 0;
 }
